0a7f910b4e090a4c3c7f555cd230debed77fe755
Kanishq2324 <kanishqjordan2324@gmail.com>
2025-03-07 19:38:46 +0530

    mini context project added

---
+++ miniContext/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useContext} from 'react'
+import userContext from '../context/UserContext'
+
+
+const Profile = () => {
+  const {user} = useContext(userContext)
+
+  if(!user) return <div>please login</div>
+
+  return <div>Welcome {user.username}</div>
+}
+
+export default Profile
